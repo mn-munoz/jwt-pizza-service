d85d0790ada1a160a7d53bbbd69f17e70bb2f84b
Merleth Munoz <merlethmunozra@gmail.com>
2026-09-23 16:28:04 -0600

    feat: add CI pipeline, testing, and linting setup
    - Updated package.json to include scripts for testing, coverage, and linting.
    - Created a CI workflow in GitHub Actions for automated builds, linting, and testing.
    - Added ESLint configuration for code quality checks.
    - Introduced Jest configuration for test coverage reporting.
    - Implemented initial tests for authentication and menu retrieval using Supertest.

---
+++ src/tests/auth.test.js
@@ -0,0 +1,30 @@
+const request = require('supertest');
+const app = require('../service');
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+});
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+
+  const user = { ...testUser, roles: [{ role: 'diner' }] };
+  delete user.password;
+  expect(loginRes.body.user).toMatchObject(user);
+});
+
+
+test('get menu as registered user', async () => {
+  const menuRes = await request(app).get('/api/order/menu').set('Authorization', `Bearer ${testUserAuthToken}`);
+  expect(menuRes.status).toBe(200);
+  expect(menuRes.body).toEqual(
+    expect.arrayContaining([expect.objectContaining({ title: 'Crusty', description: 'A dry mouthed favorite', image: 'pizza4.png', price: 0.0028 })])
+  );
+});
